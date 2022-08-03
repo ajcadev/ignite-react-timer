5130cb6e57e80ba5f993e7d7f063a5c8c328213b
Cardoso <cardosoassuncao@gmail.com>
2022-08-03 09:34:13 -0400

    add context to routes

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,8 +2,9 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 70rem;
-  height: calc(100vh - 10rem);
-  margin: 5rem auto;
+  min-height: 100%;
+  margin-left: auto;
+  margin-right: auto;
   padding: 2.5rem;
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
